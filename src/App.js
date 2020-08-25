06ebe660450bd2953f117583882fde435d29da5b
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2020-08-25 10:56:30 +0200

    init form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>1) Situation to practice</h1>
+        <Formik
+          initialValues={{
+            situation: '',
+            lastName: '',
+            email: '',
+          }}
+          onSubmit={(
+            values: Values,
+            { setSubmitting }: FormikHelpers<Values>
+          ) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 500);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Form>
+            <label htmlFor="situation">Situation</label>
+            <Field as='textarea' id="situation" name="situation" placeholder="Using a knive" />
+            <p></p>
+            <label htmlFor="date">Date</label>
+            <Field id="date" name="date" placeholder="01.01.2021" />
+            
+            <p>Please type in your SUDS rating below.</p>
+            <label htmlFor="beginning">Beginning</label>
+            <Field
+              id="beginning"
+              name="beginning"
+              placeholder="0"
+            />
+
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/ /i);
   expect(linkElement).toBeInTheDocument();
+});
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("");
+
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("");
+
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("");
 });
