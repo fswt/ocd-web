cc21a420734731254fe85e12cc509044d7dd7670
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2020-09-06 17:44:43 +0200

    expand testing

---
+++ src/Components.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { SituationFearSlider } from './Components/SituationFearSlider'
+import { SupportSwitch } from './Components/SupportSwitch'
+import { TimeFearSlider} from './Components/TimeFearSlider'
+
+describe('The SupportSwitch', () => {
+    it("renders", () => {
+      expect(2).toBe(2)
+    });
+  });
---
+++ src/ProtocolExpositionAndResponsePrevention.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProtocolExpsitionAndResponsePrevention from './ProtocolExpositionAndResponsePrevention'
+
+describe('The SupportSwitch', () => {
+    it("renders", () => {
+      expect(2).toBe(2)
+    });
+  });
